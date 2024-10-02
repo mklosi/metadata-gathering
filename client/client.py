ee04c36ed09f66cd1bb6aae2a6dd2fb55e77a28a
Martin Klosi <roise3000@gmail.com>
2024-10-01 17:41:48 -0700

    Change app port to 4000
    since the default 5000 is being used by macOS Control Center

--- client/client.py
+++ client/client.py
@@ -2,6 +2,7 @@ import os
 
 import requests
 
+BASE_URL = "http://127.0.0.1:4000"
 BASE_DIR = os.path.dirname(os.getcwd())
 CSV_FILE_NAME = 'interview.csv'
 CSV_FILE_PATH = os.path.join(BASE_DIR, CSV_FILE_NAME)
@@ -10,13 +11,13 @@ CSV_FILE_PATH = os.path.join(BASE_DIR, CSV_FILE_NAME)
 if __name__ == '__main__':
 
     # Hit 'get_metadata_csv' API route.
-    response = requests.get('http://127.0.0.1:5000/get-metadata-csv')
+    response = requests.get(f'{BASE_URL}/get-metadata-csv')
     response.raise_for_status()
     with open(CSV_FILE_PATH, 'w') as f:
         f.write(response.text)
     print(f"CSV file written to: {CSV_FILE_PATH}")
 
     # Hit 'get_metadata_json' API route. The call here is disabled (commented out), since it's not part of the reqs.
-    response = requests.get('http://127.0.0.1:5000/get-metadata-json')
+    response = requests.get(f'{BASE_URL}/get-metadata-json')
     response.raise_for_status()
     print(f"text: {response.text}") # &&&
